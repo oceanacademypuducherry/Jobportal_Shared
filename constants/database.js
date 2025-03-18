1db6c0c8dabc7e2f2a6ca0d3dc358993cf91c0be
ijassm <ijass.ocean@gmail.com>
2025-03-18 13:47:10 +0530

    FEAT: update test database name for consistency in environment configuration

--- constants/database.js
+++ constants/database.js
@@ -2,5 +2,5 @@ const { isProduction } = require("../utils/env-utils");
 
 module.exports = {
     // Database constants
-    getDatabaseName: () => isProduction() ? "OA_Job_Portal_API" : "OA_Job_Portal_TEST_API",
+    getDatabaseName: () => isProduction() ? "OA_Job_Portal_API" : "OA_Job_Portal_API_TEST",
 };
