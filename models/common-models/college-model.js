3fd4c47341f9c87bf011a2e3cbf82e4f593afc6a
ijassm <ijass.ocean@gmail.com>
2025-04-09 14:55:34 +0530

    FEAT: update college model by removing unnecessary fields and bump version to 1.0.8 in package.json

--- models/common-models/college-model.js
+++ models/common-models/college-model.js
@@ -2,24 +2,7 @@ const mongoose = require('mongoose');
 const { getDatabaseName } = require("../../constants");
 
 const collegeSchema = new mongoose.Schema({
-    universityName: {
-        type: String,
-        required: true
-    },
     collegeName: {
-        type: String,
-        required: true
-    },
-    // collegeType: {
-    //     type: String,
-    //     enum: ['Affiliated College', 'Constituent / University College', 'PG Center / Off-Campus Center', 'Recognized Center'],
-    //     required: true
-    // },
-    stateName: {
-        type: String,
-        required: true
-    },
-    districtName: {
         type: String,
         required: true
     }
