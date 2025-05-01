d10b1a9d8d0faf7a36449f6ed712d60e567251ba
Bhadresh7 <bhadreshpalani19@gmail.com>
2025-05-01 14:27:35 +0530

    Fix

---
+++ models/jobPost-models/reportJob-model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const { getDatabaseName } = require("../../constants");
+
+
+const reportSchema = new mongoose.Schema({
+    _id: { 
+        type: ObjectId, 
+        auto:true
+    },
+    candidateId: { 
+        type: ObjectId, 
+        required: true 
+    },
+    jobPostId: { 
+        type: ObjectId, 
+        required: true 
+    }, 
+    reportReason:{
+        type: Array,
+        default: []
+    }
+})
+const myDB = mongoose.connection.useDb(getDatabaseName());
+// Create the model
+const JobReportModel = myDB.model("job-report", reportSchema);
+
+module.exports = JobReportModel;
